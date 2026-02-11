5fef83b90ca1eb9389cbfea1e3fcb5ee80943cc1
for-retail <narutohina1411@gmail.com>
2026-02-11 09:39:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,25 @@
-const CACHE_NAME = 'vault-v1';
+const CACHE_NAME = 'vault-cache-v1';
 const ASSETS = [
   './',
   './index.html',
+  './manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+// Install: Website files ni cache loki lakkuntundhi
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// Fetch: Net lekapothe cache nundi website chupisthundhi
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });

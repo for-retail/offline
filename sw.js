ea0f8b776d1f5f89e6a2867d3523cdb6ab5222ca
for-retail <narutohina1411@gmail.com>
2026-02-11 10:48:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,20 @@
-const CACHE_NAME = 'vault-v1';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'vault-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });

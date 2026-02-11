51c4357aa8a65fe12be6bd68327444ca153f7795
for-retail <narutohina1411@gmail.com>
2026-02-11 09:34:17 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,16 @@
-const CACHE_NAME = 'vault-offline-v1';
+const CACHE_NAME = 'vault-v1';
 const ASSETS = [
   './',
   './index.html',
-  './manifest.json',
   'https://cdn.tailwindcss.com'
 ];
 
-// Install: Website code ni browser memory lo dhasthundi
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS);
-    })
-  );
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-// Fetch: Net lekapothe dhasina code ni bayataki thesthundhi
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
